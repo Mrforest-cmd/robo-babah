92fe769c59b731cd9ec3b53eac2fa8a537fa2f2e
Yaroslav664 <steamtopyarfire@gmail.com>
2024-01-16 08:35:52 +0200

    code changes

--- Application/Application/Application.cpp
+++ Application/Application/Application.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -7,19 +8,21 @@ class Robot {
     unsigned short energy;
     unsigned short attack;
     unsigned short protection;
-    string strategy = "balance";
+    string strategy;
 
 public:
-    Robot(unsigned short health=100, unsigned short energy=100, unsigned short attack=20, unsigned short protection=50){
+    Robot(unsigned short health = 100, unsigned short energy = 100, unsigned short attack = 20, unsigned short protection = 50, string strategy = "balance") {
         this->health = health;
         this->energy = energy;
         this->attack = attack;
         this->protection = protection;
+        this->strategy = strategy;
     }
-    unsigned short gethealth() {return health;}
-    unsigned short getenergy() { return health; }
-    unsigned short getprotection() { return health; }
-    unsigned short getattack() { return health; }
+    unsigned short gethealth() { return health; }
+    unsigned short getenergy() { return energy; }
+    unsigned short getprotection() { return protection; }
+    unsigned short getattack() { return attack; }
+    string getstrategy() { return strategy; }
     void takedamage(unsigned int damage) {
         this->health -= damage;
     }
@@ -27,7 +30,7 @@ public:
         this->protection -= damage;
     }
 
-    void attack(Robot enemy) {
+    void attacking(Robot& enemy) {
         if (enemy.getprotection() > attack) {
             enemy.protectdamage(attack);
         }
@@ -43,16 +46,17 @@ public:
     void protect() {
         energy -= 5;
         protection += 5;
+        attack -= 5;
     }
 
-    void stats(){
+    void stats() {
         cout << "Hp: " << health << endl
             << "Protection: " << protection << endl
             << "Attack: " << attack << endl
-            << "Energy: " << energy << endl;
+            << "Energy: " << energy << endl << endl;
     }
 
-    void pickstrategy(char a) {
+ /*   void pickstrategy(char a) {
         switch (a) {
         case('a'):
             strategy = "aggresive";
@@ -67,26 +71,87 @@ public:
             strategy = "balance";
         }
     }
+    */
+};
+
+class Arena {
+public:
+
+    void Battle(Robot& robot1, Robot& robot2) {
+        cout << "Battle started!\n";
+
+        while (robot1.gethealth() > 0 && robot2.gethealth() > 0) {
+
+            cout << "Robot 1's turn:\n";
+
+            if (robot1.getstrategy() == "aggressive") {
+                robot1.attacking(robot2);
+                cout << "Attacking!\n";
+            }
+            else if (robot1.getstrategy() == "defensive") {
+                robot1.protect();
+                cout << "Defending!\n";
+            }
+            else {
+                robot1.attacking(robot2);
+                cout << "Attacking!\n";
+            }
+
+            robot1.stats();
+            robot2.stats();
+
+            if (robot2.gethealth() <= 0) {
+                break;
+            }
+
+            cout << "Robot 2's turn:\n";
+
+            if (robot2.getstrategy() == "aggressive") {
+                robot2.attacking(robot1);
+                cout << "Attacking!\n";
+            }
+            else if (robot2.getstrategy() == "defensive") {
+                robot2.protect();
+                cout << "Defending!\n";
+            }
+            else {
+                robot2.attacking(robot1);
+                cout << "Attacking!\n";
+            }
+
+            robot1.stats();
+            robot2.stats();
+        }
+
+        if (robot1.gethealth() <= 0) {
+            cout << "Robot 2 wins the battle!\n";
+        }
+        else {
+            cout << "Robot 1 wins the battle!\n";
+        }
+    }
 };
 
 int main()
 {
-    
+    Robot robot1 = Robot(100, 100, 20, 50, "aggressive");
+    Robot robot2 = Robot(100, 100, 20, 50, "balance");
+    Arena arena;
+    arena.Battle(robot1, robot2);
 }
 
-
 /*
-Для 2х студентів Ваша група має створити симулятор битви між роботами. 
-Кожен член групи відповідає за свою частину проекту: 
+Для 2х студентів Ваша група має створити симулятор битви між роботами.
+Кожен член групи відповідає за свою частину проекту:
 
-Перший учасник: Реалізувати клас "Робот" із основними характеристиками: здоров'я, енергія, атака, захист. 
+Перший учасник: Реалізувати клас "Робот" із основними характеристиками: здоров'я, енергія, атака, захист.
 Методи для атаки та захисту, що враховують поточні характеристики робота.  Done*
-Можливість вибору стратегії бою (наприклад, агресивна, оборонна, баланс). 
+Можливість вибору стратегії бою (наприклад, агресивна, оборонна, баланс).
 
-Другий учасник: Реалізувати клас "Арена" щодо битви між роботами. 
-Методи для початку та закінчення бою, відстеження стану кожного робота. 
-Логіка визначення переможця та виведення результатів битви. 
-Надайте приклади використання вашого симулятора, де роботи з різними характеристиками борються один з одним. 
+Другий учасник: Реалізувати клас "Арена" щодо битви між роботами.
+Методи для початку та закінчення бою, відстеження стану кожного робота.
+Логіка визначення переможця та виведення результатів битви.
+Надайте приклади використання вашого симулятора, де роботи з різними характеристиками борються один з одним.
 
 Використовуйте вказівники для обміну даними між класами.
 */
